50bd11ff4aa9d0faa8ce1d4095661a84cacfe160
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-11-05 20:27:42 +0500

    Add hibernate config and Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+
+import javax.persistence.metamodel.EntityType;
+
+import utils.HibernateUtil;
+
 public class Main {
     public static void main(final String[] args) throws Exception {
-
+        final Session session = HibernateUtil.getOpenSession();
+        try {
+            System.out.println("querying all the managed entities...");
+            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                final String entityName = entityType.getName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
+        }
     }
 }
